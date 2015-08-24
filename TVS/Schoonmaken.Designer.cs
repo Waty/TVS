8d439911fcca52b702007aa69eed3862ee611134
davyalders <davyalders@gmail.com>
2015-08-24 15:35:52 +0200

    Forms toegevoegd

---
+++ TVS/Schoonmaken.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TVS
+{
+    partial class Schoonmaken
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTerug = new System.Windows.Forms.Button();
+            this.lbWagen = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTerug
+            // 
+            this.btnTerug.Location = new System.Drawing.Point(387, 345);
+            this.btnTerug.Name = "btnTerug";
+            this.btnTerug.Size = new System.Drawing.Size(75, 23);
+            this.btnTerug.TabIndex = 0;
+            this.btnTerug.Text = "Terug";
+            this.btnTerug.UseVisualStyleBackColor = true;
+            this.btnTerug.Click += new System.EventHandler(this.btnTerug_Click);
+            // 
+            // lbWagen
+            // 
+            this.lbWagen.AutoSize = true;
+            this.lbWagen.Location = new System.Drawing.Point(12, 9);
+            this.lbWagen.Name = "lbWagen";
+            this.lbWagen.Size = new System.Drawing.Size(48, 13);
+            this.lbWagen.TabIndex = 1;
+            this.lbWagen.Text = "Wagen: ";
+            // 
+            // Schoonmaken
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 377);
+            this.Controls.Add(this.lbWagen);
+            this.Controls.Add(this.btnTerug);
+            this.Name = "Schoonmaken";
+            this.Text = "Schoonmaken";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTerug;
+        private System.Windows.Forms.Label lbWagen;
+    }
+}
