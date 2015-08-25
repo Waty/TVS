329b47d05b23de285bd3445389ec9aa4c0ed0ce5
Aarnoutse,Wart W.J <w.aarnoutse@student.fontys.nl>
2015-08-25 09:59:37 +0200

    Cleaned MainForm.cs

--- TVS/MainForm.cs
+++ TVS/MainForm.cs
@@ -1,54 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TVS
 {
     /// <summary>
-    /// Main form
+    ///     Main form
     /// </summary>
     public partial class MainForm : Form
     {
         /// <summary>
-        /// Constructor
+        ///     Constructor
         /// </summary>
         public MainForm()
         {
             InitializeComponent();
         }
+
         /// <summary>
-        /// Het inloggen van een gebruiker.
+        ///     Het inloggen van een gebruiker.
         /// </summary>
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            lbGebruikersNaam.Visible = false;
-            lbWachtwoord.Visible = false;
-            tbLoginNaam.Visible = false;
-            tbLoginWachtwoord.Visible = false;
-            btnLogin.Visible = false;
-            btnLoguit.Visible = true;
-            lbGebruiker.Visible = true;
-            pnMain.Visible = true;
+            ToggleLoggedInState(true);
         }
+
         /// <summary>
-        /// Het uitloggen van een gebruiker.
+        ///     Het uitloggen van een gebruiker.
         /// </summary>
         private void btnLoguit_Click(object sender, EventArgs e)
         {
-            lbGebruikersNaam.Visible = true;
-            lbWachtwoord.Visible = true;
-            tbLoginNaam.Visible = true;
-            tbLoginWachtwoord.Visible = true;
-            btnLogin.Visible = true;
-            btnLoguit.Visible = false;
-            lbGebruiker.Visible = false;
-            pnMain.Visible = false;
+            ToggleLoggedInState(false);
+        }
+
+        /// <summary>
+        ///     Changes the UI to reflect the logged in state
+        /// </summary>
+        /// <param name="loggedIn">if true the login box is hidden</param>
+        private void ToggleLoggedInState(bool loggedIn)
+        {
+            lbGebruikersNaam.Visible = !loggedIn;
+            lbWachtwoord.Visible = !loggedIn;
+            tbLoginNaam.Visible = !loggedIn;
+            tbLoginWachtwoord.Visible = !loggedIn;
+            btnLogin.Visible = !loggedIn;
+            btnLoguit.Visible = loggedIn;
+            lbGebruiker.Visible = loggedIn;
+            pnMain.Visible = loggedIn;
         }
     }
 }
