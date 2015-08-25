d0ded4b7945d2c5a12684480a0087eaec6e8b3d3
Aarnoutse,Wart W.J <w.aarnoutse@student.fontys.nl>
2015-08-25 11:34:43 +0200

    Added a simple LoginDialog

---
+++ TVS/LoginDialog.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TVS
+{
+    partial class LoginDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.lUsername = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.bLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(12, 25);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 20);
+            this.tbUsername.TabIndex = 0;
+            // 
+            // lUsername
+            // 
+            this.lUsername.AutoSize = true;
+            this.lUsername.Location = new System.Drawing.Point(12, 9);
+            this.lUsername.Name = "lUsername";
+            this.lUsername.Size = new System.Drawing.Size(55, 13);
+            this.lUsername.TabIndex = 1;
+            this.lUsername.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(170, 25);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 3;
+            // 
+            // bLogin
+            // 
+            this.bLogin.Location = new System.Drawing.Point(12, 51);
+            this.bLogin.Name = "bLogin";
+            this.bLogin.Size = new System.Drawing.Size(260, 23);
+            this.bLogin.TabIndex = 4;
+            this.bLogin.Text = "Login";
+            this.bLogin.UseVisualStyleBackColor = true;
+            this.bLogin.Click += new System.EventHandler(this.bLogin_Click);
+            // 
+            // LoginDialog
+            // 
+            this.AcceptButton = this.bLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 82);
+            this.Controls.Add(this.bLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lUsername);
+            this.Controls.Add(this.tbUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "LoginDialog";
+            this.Text = "LoginDialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label lUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button bLogin;
+    }
+}
