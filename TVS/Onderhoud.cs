284027aa821b71da178cec5e0f74bdb123ce929f
davyalders <davyalders@gmail.com>
2015-08-26 13:33:58 +0200

    Onderhoud en Tram

---
+++ TVS/Onderhoud.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVS
+{
+    /// <summary>
+    ///     Maintenance and/or repairs
+    /// </summary>
+    class Onderhoud
+    {
+        /// <summary>
+        ///     Enum for the type of maintenance
+        /// </summary>
+        public enum TypeOnderhoud
+        {
+            GroteServicebeurt,
+            KleineServicebeurt,
+            Reparatie
+        }
+       /// <summary>
+       ///     Timeindication of repairs
+       /// </summary>
+        public DateTime Tijd { get; set; }
+       /// <summary>
+       ///      ID of the Tram being repaired
+       /// </summary>
+        public int TramId { get; set; }
+        /// <summary>
+        ///     The technician 
+        /// </summary>
+        public Medewerker Medewerker { get; set; }
+        /// <summary>
+        ///     Type of Maintenance
+        /// </summary>
+        public TypeOnderhoud Type { get; set; }
+
+        /// <summary>
+        ///     constructor
+        /// </summary>
+        /// <param name="medewerker"> The technician</param>
+        /// <param name="date"> the indicated time when this is finished</param>
+        /// <param name="tramid">ID of the Tram</param>
+        /// <param name="type">Type of maintenance</param>
+        public Onderhoud(Medewerker medewerker, DateTime date, int tramid, TypeOnderhoud type )
+        {
+            Medewerker = medewerker;
+            Tijd = date;
+            TramId = tramid;
+            Type = type;
+        }
+    }
+    
+}
--- TVS/Tram.cs
+++ TVS/Tram.cs
@@ -1,4 +1,6 @@
-﻿namespace TVS
+﻿using Microsoft.SqlServer.Server;
+
+namespace TVS
 {
     /// <summary>
     ///     Class to create or keep track of a Tram
@@ -6,59 +8,77 @@
     public class Tram
     {
         /// <summary>
-        /// Tram ID
+        ///     Tram ID
         /// </summary>
         public int Id { get; set; }
 
         /// <summary>
-        /// Tram number
+        ///     Tram number
         /// </summary>
         public int Nummer { get; set; }
 
         /// <summary>
-        /// Tram lenght
+        ///     Tram lenght
         /// </summary>
         public int Lengte { get; set; }
 
         /// <summary>
-        /// Shows whether the Tram needs to be cleaned.
+        ///     Shows whether the Tram needs to be cleaned.
         /// </summary>
         public bool Vervuild { get; set; }
 
         /// <summary>
-        /// Shows whether the Tram needs to be repaired.
+        ///     Shows whether the Tram needs to be repaired.
         /// </summary>
         public bool Defect { get; set; }
 
         /// <summary>
-        /// Shows whether the Tram is suited fo have a "Conducteur"
+        ///     Shows whether the Tram is suited fo have a "Conducteur"
         /// </summary>
         public bool ConductGeschikt { get; set; }
 
         /// <summary>
-        /// Shows whether the Tram is available
+        ///     Shows whether the Tram is available
         /// </summary>
         public bool Beschikbaar { get; set; }
 
         /// <summary>
-        /// Shows the ID of the Tram type
+        ///     Shows the ID of the Tram type
         /// </summary>
         public int TramTypeID { get; set; }
 
         /// <summary>
-        /// Shows the ID of the "Remise" it belongs to.
+        ///     Shows the ID of the "Remise" it belongs to.
         /// </summary>
         public int RemiseId { get; set; }
 
         /// <summary>
-        /// Geeft de status van de Tram aan
+        ///     Constructor
         /// </summary>
-        public Status Status { get; set; }
+        public Tram(int id, int nummer, int lengte, bool vervuild, bool defect, bool conductGeschikt, bool beschikbaar, int tramtypeid, int remiseid)
+        {
+            Id = id;
+            Nummer = nummer;
+            Lengte = lengte;
+            Vervuild = vervuild;
+            Defect = defect;
+            ConductGeschikt = conductGeschikt;
+            Beschikbaar = beschikbaar;
+            TramTypeID = tramtypeid;
+            RemiseId = remiseid;
+        }
+        /// <summary>
+        ///     Empty constructor
+        /// </summary>
+        public Tram()
+        {
+            
+        }
 
         /// <summary>
-        /// ToString function of the class.
+        ///     ToString function of the class.
         /// </summary>
-        /// <returns>Returns the data in a string</returns>
+        /// <returns>   Returns the data in a string</returns>
         public override string ToString()
         {
             return "ID: " + Id + ", Nummer: " + Nummer + "";
