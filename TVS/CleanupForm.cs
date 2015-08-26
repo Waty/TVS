43936dd6562246d90015aa7c2744bbdf2f0742ee
Aarnoutse,Wart W.J <w.aarnoutse@student.fontys.nl>
2015-08-26 10:04:19 +0200

    Added more comments to the CleanupForm and added loading on startup

--- TVS/CleanupForm.cs
+++ TVS/CleanupForm.cs
@@ -5,21 +5,34 @@ using System.Windows.Forms;
 namespace TVS
 {
     /// <summary>
+    ///     The form for the people that need to clean the tram
     /// </summary>
     public partial class CleanupForm : Form
     {
         /// <summary>
+        ///     Constructs the default CleanupForm
         /// </summary>
         public CleanupForm()
         {
             InitializeComponent();
         }
 
-        private void Schoonmaak_Load(object sender, EventArgs e) {}
 
-        private void btnSchoonmaakLijst_Click(object sender, EventArgs e)
+        /// <summary>
+        ///     Updates the listbox with the most recent data from the database
+        /// </summary>
+        private void LoadCleanupData(object sender, EventArgs e)
         {
-            lbSchoonmaak.DataSource = Database.GetAllTrams().Where(tram => tram.Vervuild).ToList();
+            lbCleanupTasks.DataSource = Database.GetAllTrams().Where(tram => tram.Vervuild).ToList();
+        }
+
+        /// <summary>
+        ///     Saves a new cleanup to the database and refreshes the cleanup list
+        /// </summary>
+        private void btnRegisterCleanup_Click(object sender, EventArgs e)
+        {
+            //TODO: save the cleanup in the database
+            LoadCleanupData(sender, e);
         }
     }
 }
