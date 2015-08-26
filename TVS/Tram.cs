d96dbe83504d6260633d2480daa01bb507e4f694
davyalders <davyalders@gmail.com>
2015-08-26 12:09:19 +0200

    Commentaar aan Tram.CS

--- TVS/Tram.cs
+++ TVS/Tram.cs
@@ -6,30 +6,37 @@
     public class Tram
     {
         /// <summary>
+        /// ID van de Tram
         /// </summary>
         public int ID { get; set; }
 
         /// <summary>
+        /// Nummer van de Tram
         /// </summary>
         public int Nummer { get; set; }
 
         /// <summary>
+        /// Lengte van de Tram
         /// </summary>
         public int Lengte { get; set; }
 
         /// <summary>
+        /// Geeft aan of the Tram moet worden schoongemaakt.
         /// </summary>
         public bool Vervuild { get; set; }
 
         /// <summary>
+        /// Geeft aan of de trein moet worden gerepareerd.
         /// </summary>
         public bool Defect { get; set; }
 
         /// <summary>
+        /// Geeft aan of de Tram geschikt is om een conducteur bij te hebben.
         /// </summary>
         public bool ConductGeschikt { get; set; }
 
         /// <summary>
+        /// Geeft aan of dat de Tram beschikbaar is
         /// </summary>
         public bool Beschikbaar { get; set; }
 
@@ -38,16 +45,19 @@
         public int TramTypeID { get; set; }
 
         /// <summary>
+        /// Geeft het TypeID van de Tram aan.
         /// </summary>
         public int RemiseId { get; set; }
 
         /// <summary>
+        /// Geeft de status van de Tram aan
         /// </summary>
         public Status Status { get; set; }
 
         /// <summary>
+        /// De ToString van de Tram
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Geeft een string met de data van de Tram</returns>
         public override string ToString()
         {
             return "ID: " + ID + ", Nummer: " + Nummer + "";
