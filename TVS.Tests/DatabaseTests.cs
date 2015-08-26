f0ba4d2efd89657df9fe4606128dbedf07babc70
Aarnoutse,Wart W.J <w.aarnoutse@student.fontys.nl>
2015-08-26 10:03:09 +0200

    Added a simple unittest project

---
+++ TVS.Tests/DatabaseTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TVS.Tests
+{
+    /// <summary>
+    ///     Test for the <see cref="Database" /> class, should only be used with the default test data from the
+    ///     createscript.sql
+    /// </summary>
+    [TestClass]
+    public class DatabaseTests
+    {
+        [TestMethod]
+        public void DataLoginTest()
+        {
+            Assert.AreEqual("Schoonmaker", Database.Login("Sjaak_Schoonmaker", "TestTest1"));
+
+            Assert.AreEqual(null, Database.Login("Invalid", "Login"));
+        }
+    }
+}
