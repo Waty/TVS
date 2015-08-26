4a652dfb034eac3dc8099a0f83368d09c2543d72
davyalders <davyalders@gmail.com>
2015-08-26 14:07:58 +0200

    Added Remise and Administration

---
+++ TVS/Remise.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVS
+{
+    /// <summary>
+    ///     Class for a Remise
+    /// </summary>
+    public class Remise
+    {
+        /// <summary>
+        ///     ID of the remise
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        ///     Name of the remise
+        /// </summary>
+        public string Naam { get; set; }
+
+        /// <summary>
+        ///     Large cleaning
+        /// </summary>
+        public int GroteSchoonmaakbeurt { get; set; }
+
+        /// <summary>
+        ///     Small cleaning
+        /// </summary>
+        public int KleineSchoonmaakbeurt { get; set; }
+
+        /// <summary>
+        ///     Large service
+        /// </summary>
+        public int GroteServicebeurt { get; set; }
+
+        /// <summary>
+        ///     Small sevice
+        /// </summary>
+        public int KleineServicebeurt { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Remise(int id, string naam, int groteSc, int kleineSc, int groteSe, int kleineSe)
+        {
+            Id = id;
+            Naam = naam;
+            GroteSchoonmaakbeurt = groteSc;
+            KleineSchoonmaakbeurt = kleineSc;
+            GroteServicebeurt = groteSe;
+            KleineServicebeurt = kleineSe;
+        }
+        /// <summary>
+        ///     Empty Constructor
+        /// </summary>
+        public Remise()
+        {
+            
+        }
+    }
+}
