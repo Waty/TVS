057c862763e4477b5708aa3a8324fd088e25b1bc
davyalders <davyalders@gmail.com>
2015-08-27 12:00:48 +0200

    Maintenane aanpassing

--- TVS/Models/Maintenance.cs
+++ TVS/Models/Maintenance.cs
@@ -33,25 +33,32 @@ namespace TVS.Models
             Tram = tram;
             Type = type;
         }
+        /// <summary>
+        ///     Empty constructor
+        /// </summary>
+        public Maintenance()
+        {
+            throw new NotImplementedException();
+        }
 
         /// <summary>
         ///     The <see cref="Medewerker" /> that executed the mainenance
         /// </summary>
-        public Medewerker Medewerker { get; }
+        public Medewerker Medewerker { get; set; }
 
         /// <summary>
         ///     The date of execution
         /// </summary>
-        public DateTime Date { get; }
+        public DateTime Date { get; set; }
 
         /// <summary>
         ///     The tram that was maintained
         /// </summary>
-        public Tram Tram { get; }
+        public Tram Tram { get; set; }
 
         /// <summary>
         ///     The resulting type of the maintenance
         /// </summary>
-        public MaintenanceType Type { get; }
+        public MaintenanceType Type { get; set; }
     }
 }
